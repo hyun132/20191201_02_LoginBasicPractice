081af59be303bba056ca1a32901cdcd5c2fa6636
미현 김 <rlaalgus132@naver.com>
2019-12-01 13:57:14 +0900

    사용자정보 액티비티 추가 생성 해당 액티비티로 이동(Intent)

--- app/src/main/java/com/tjoeun/a20191201_02_loginbasicpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191201_02_loginbasicpractice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tjoeun.a20191201_02_loginbasicpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -11,8 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         loginBtn.setOnLongClickListener {
             var inputId = idEdt.text.toString()
             var inputPw = pwEdt.text.toString()
@@ -20,9 +19,15 @@ class MainActivity : AppCompatActivity() {
 //            id : admin => pw123 관리자입니다.  비번이 틀리면 "관리자 비번이 틀렸습니다."
 
             if (inputId=="admin"){
+
                 if(inputPw=="pw123"){
                     Toast.makeText(this,"관리자입니다.",Toast.LENGTH_SHORT).show()
-                }else{
+
+                    val intent =Intent(this,UserInfoActivity::class.java)
+                    startActivity(intent)
+
+                }
+                else{
                     Toast.makeText(this,"관리자 비번이 틀렸습니다.",Toast.LENGTH_SHORT).show()
                 }
             }
---
+++ app/src/main/java/com/tjoeun/a20191201_02_loginbasicpractice/UserInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191201_02_loginbasicpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+}
