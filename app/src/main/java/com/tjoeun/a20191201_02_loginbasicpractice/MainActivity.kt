c5e3030da62a4f38a32cd27926e7d68b6360a7e7
미현 김 <rlaalgus132@naver.com>
2019-12-01 12:33:19 +0900

    로그인 로직 수정 -id 검사..?

--- app/src/main/java/com/tjoeun/a20191201_02_loginbasicpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191201_02_loginbasicpractice/MainActivity.kt
@@ -2,11 +2,37 @@ package com.tjoeun.a20191201_02_loginbasicpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        loginBtn.setOnLongClickListener {
+            var inputId = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+
+//            id : admin => pw123 관리자입니다.  비번이 틀리면 "관리자 비번이 틀렸습니다."
+
+            if (inputId=="admin"){
+                if(inputPw=="pw123"){
+                Toast.makeText(this,"관리자입니다.",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"관리자 비번이 틀렸습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this,"일반사용자입니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
+
 }
+
